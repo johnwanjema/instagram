513bae4a371bb09847c88bc4a5a77832e6299cbc
johnwanjema <jonwanjema@gmail.com>
2019-07-27 13:42:03 +0300

    added url for login,profile

--- ig/urls.py
+++ ig/urls.py
@@ -6,10 +6,13 @@ from django.contrib.auth import views as auth_views
 from django.contrib import admin
 
 urlpatterns=[
-    url('^$',views.index,name = 'index'),
+    url('^home/$',views.index,name = 'index'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^logout/$',auth_views.logout, {"next_page": '/'}), 
+    url(r'^$',views.signup, name='signup'),  
+    url(r'^logout/$',auth_views.logout, {"next_page": '/'}),
+    url(r'^login/$',auth_views.login, {"next_page": '/'}), 
     url(r'^admin/', admin.site.urls),
+    url('profile/', views.index, name='profile'),
     url(r'^user/(?P<username>\w+)', views.profile, name='profile'),
     ]
 if settings.DEBUG:
